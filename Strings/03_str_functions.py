33c53dfe73bb8415cf4eb333e3902a7415277f59
Soham Yeola <sohamm3176@gmail.com>
2025-12-14 01:13:33 +0530

    types of strings

---
+++ Strings/03_str_functions.py
@@ -0,0 +1,10 @@
+name = "harry"
+
+print(len(name))
+print(name.endswith("rry"))
+print(name.startswith("ha"))
+print(name.capitalize())
+print(name.count("r"))
+print(name.find("rr"))
+replaced_string = name.replace("r", "l")
+print(replaced_string)
---
+++ Strings/04_escape_seq.py
@@ -0,0 +1,3 @@
+a  = "Soham is good boy\nbut not a bad \"boy\""
+
+print(a)
