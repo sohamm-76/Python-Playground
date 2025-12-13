ae217f931d55a6494bc583c300d6a707beb2f52a
Soham Yeola <sohamm3176@gmail.com>
2025-12-14 01:11:44 +0530

    variables & datatypes

---
+++ Variables & datatypes/05_type.py
@@ -0,0 +1,11 @@
+a = "31.2"
+b = float(a)  # a but the type should be float
+t = type(a)
+
+print(t)
+
+a = "31.2"
+b = float(a)  
+t = type(b)
+
+print(t)
---
+++ Variables & datatypes/06_input.py
@@ -0,0 +1,15 @@
+#Type 1
+a = input("Enter the number a: ")
+b = input("Enter the number b: ")
+
+print("Number a is : ", a)
+print("Number b is : ", b)
+print("Sum is ", a + b)
+
+#Type 2
+a = int(input("Enter the number a: "))
+b = int(input("Enter the number b: "))
+
+print("Number a is : ", a)
+print("Number b is : ", b)
+print("Sum is ", a + b)
