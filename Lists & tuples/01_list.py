74eba00dcc80fb646b1cb6257ffc3c382f9358ce
Soham Yeola <sohamm3176@gmail.com>
2025-12-16 13:59:34 +0530

    add python lists and tuples

---
+++ Lists & tuples/01_list.py
@@ -0,0 +1,7 @@
+friends = ["Apple", "Orange", 5, 345.07, False, "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0]="Grapes"#Unlike strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
---
+++ Lists & tuples/03_tuple.py
@@ -0,0 +1,10 @@
+a = () # empty tuple
+a = (1,) # tuple with only one element needs a comma
+a = (1,7,2) # tuple with more than one element
+
+print(a)
+print(type(a))
+
+b = (1,45,342,3424,False, "Rohan", "Shivam")
+print(b)
+print(type(b))
