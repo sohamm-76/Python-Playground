58824285128fa299b18e00a55d2d03ad4435ad2c
Soham Yeola <sohamm3176@gmail.com>
2025-12-19 20:34:48 +0530

    Add files via upload

---
+++ Conditional Expressions/03_multiple_statements.py
@@ -0,0 +1,23 @@
+a = int(input("Enter your age: "))
+
+# If statement no: 1
+if(a%2 == 0):
+    print("a is even")
+# End of If statement no: 2    
+    
+# If statement no: 2
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering a invalid negative age")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+    
+else:
+    print("You are below the age of consent")
+# End of If statement no: 2
+
+print("End of program!")
