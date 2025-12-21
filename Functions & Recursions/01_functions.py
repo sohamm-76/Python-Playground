a9d626b0d3ac3e8b37c14c92c1343881e3fd8da8
Soham Yeola <sohamm3176@gmail.com>
2025-12-21 22:31:56 +0530

    add functions and recursion in python

---
+++ Functions & Recursions/01_functions.py
@@ -0,0 +1,31 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+ 
+# average = (a + b + c)/3
+# print(average)
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+ 
+# average = (a + b + c)/3
+# print(average)
+
+#Function definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    
+    average= (a+b+c)/3
+    print(average)
+
+avg()#Function call
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
---
+++ Functions & Recursions/03_functions_with_arguements.py
@@ -0,0 +1,7 @@
+def goodDay(name,ending):
+    print("Good Day, "+name)
+    print(ending)
+    
+    
+goodDay("Harry","Thank you")
+goodDay("Rohan","Thanks")
