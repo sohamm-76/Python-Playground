523e7647667b7f1d0524fffb9e91b8d00664f088
Soham Yeola <sohamm3176@gmail.com>
2026-01-20 23:47:51 +0530

    problems on lists and tuples

---
+++ Lists & tuples practice set/03_probelm3.py
@@ -0,0 +1,5 @@
+# Check that a tuple type cannot be changed in python.
+
+a = (34, 234, "Harry")
+
+a = (a[0], a[1], "Larry")
---
+++ Lists & tuples practice set/04_problem4.py
@@ -0,0 +1,4 @@
+# Write a program to sum a list with 4 numbers. 
+
+l = (3 , 3, 5 ,1)
+print(sum(l))
